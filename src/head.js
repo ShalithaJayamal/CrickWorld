36f9172c345dd898979fa089e60620ba9852e1dc
ssss <shalithajayamal96@gmail.com>
2020-05-18 15:13:47 +0530

    head.js created

---
+++ src/head.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Layout, Avatar} from 'antd';
+import { Menu, Button } from 'antd';
+import { Typography } from 'antd';
+import { SearchOutlined } from '@ant-design/icons'
+import User from './users'
+import './App.css';
+
+
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+const { Title } = Typography;
+
+const {Header,Footer,Sider,Content } = Layout;
+
+
+class Head extends React.Component {
+    render(){
+  return (
+
+      <Layout>
+
+      </Layout>
+    
+  );
+}
+}
+
+export default Head;
