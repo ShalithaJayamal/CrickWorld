5bf9dce92f61d4c128c8d7381b84bce85fb745b1
ssss <shalithajayamal96@gmail.com>
2020-05-19 23:57:03 +0530

    add some test files

--- src/head.js
+++ src/head.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Layout, Avatar} from 'antd';
+import { Layout, Avatar, Divider} from 'antd';
 import { Menu, Button } from 'antd';
 import { Typography } from 'antd';
 import { SearchOutlined } from '@ant-design/icons'
 import User from './users'
 import './App.css';
+
 
 
 import {
@@ -15,6 +16,7 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   MailOutlined,
+  FacebookOutlined,
 } from '@ant-design/icons';
 import { UserOutlined } from '@ant-design/icons';
 
@@ -29,9 +31,18 @@ class Head extends React.Component {
     render(){
   return (
 
+      <div className = "Sha">
       <Layout>
-
+        Shalitha
+      <div className= "a">
+      <FacebookOutlined />
+      </div>
       </Layout>
+     
+      
+      
+
+      </div>
     
   );
 }
